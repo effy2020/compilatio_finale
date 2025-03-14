0860cf3b21662353062dc5d4ad869142690be348
feriel <hadjarabferiel@gmail.com>
2025-03-15 00:09:56 +0100

    modif main

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Entrez un programme JS à parser");
+        System.out.println("Entrez un programme JS à parser : ");
     }
 }
