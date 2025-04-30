9b9427fd82f8365511526e4591d7bd09eb3b4b69
feriel <hadjarabferiel@gmail.com>
2025-04-30 16:36:57 +0200

    modif

--- AST/Eql.java
+++
@@ -1,8 +0,0 @@
-package AST;
-
-public class Eql extends ExpressionA_Binaire{
-    public Eql(ExpressionA gauche,ExpressionA droite) {super(gauche, droite);}
-    
-    public String symbole() {return "Eq";}; 
-}
- 
--- AST/GrStlNb.java
+++
@@ -1,8 +0,0 @@
-package AST;
-
-public class GrStlNb extends ExpressionA_Binaire{
-    public GrStlNb(ExpressionA gauche,ExpressionA droite) {super(gauche, droite);}
-    
-    public String symbole() {return "GrStlNb";}; 
-}
- 
